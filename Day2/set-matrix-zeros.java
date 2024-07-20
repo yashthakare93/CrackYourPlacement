0f4e598c29e50ed6dfc9e2a98ceb8431bd891e72
Yash Thakare <thakareyash74@gmail.com>
2024-07-20 19:55:19 +0530

    Day 2 : Solved 5 questions

---
+++ Day2/set-matrix-zeros.java
@@ -0,0 +1,14 @@
+package Day2;
+
+class Solution {
+    public int removeDuplicates(int[] ar) {
+        int j=0;
+        for(int i=1;i<ar.length;i++){
+            if(ar[j] != ar[i]){
+                j++;
+                ar[j]=ar[i];
+            }
+        }
+         return j+1;
+    }
+}

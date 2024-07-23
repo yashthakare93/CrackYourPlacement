e42ecd25a1052db8f53fe076fba380b7394bae41
Yash Thakare <thakareyash74@gmail.com>
2024-07-23 19:01:39 +0530

    Day 5 : Solved 3 questions

---
+++ Day5/simplify-path.java
@@ -0,0 +1,26 @@
+package Day5;
+
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] directories = path.split("/");
+        for (String dir : directories) {
+            if (dir.equals(".") || dir.isEmpty()) {
+                continue;
+            } else if (dir.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(dir);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (String dir : stack) {
+            result.append("/").append(dir);
+        }
+        return result.length() > 0 ? result.toString() : "/";
+    }
+}

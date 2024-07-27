f7f1ffffe468c274c49fd2667602ee7a8a220a8a
Yash Thakare <thakareyash74@gmail.com>
2024-07-27 20:55:21 +0530

    Day 9 : Solved 5 questions

---
+++ Day9/excel-sheet-column-title.java
@@ -0,0 +1,14 @@
+package Day9;
+
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            char c = (char) ('A' + columnNumber % 26);
+            sb.insert(0, c);
+            columnNumber /= 26;
+        }
+        return sb.toString();
+    }
+}
---
+++ Day9/minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,17 @@
+package Day9;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+
+        int mid = nums[nums.length / 2];
+        int count = 0;
+
+        for (int num : nums)
+            count += Math.abs(mid - num);
+
+        return count;
+    }
+}
